8841bcb8e4545f1abc85aa145e0e1812714d410b
stinboslice <stinboslice@outlook.com>
2026-04-24 13:08:19 -0400

    Create game.js

---
+++ src/game/game.js
@@ -0,0 +1,21 @@
+import Phaser from "phaser";
+import { BootScene } from "./scenes/BootScene.js";
+
+export function createGame() {
+  const config = {
+    type: Phaser.AUTO,
+    parent: "game-root",
+    width: 1280,
+    height: 720,
+    backgroundColor: "#08080c",
+    pixelArt: true,
+    roundPixels: true,
+    scene: [BootScene],
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+  };
+
+  return new Phaser.Game(config);
+}
