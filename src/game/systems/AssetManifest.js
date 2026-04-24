84b0976e84a5e15216c39c920d15171cc484f1a0
stinboslice <stinboslice@outlook.com>
2026-04-24 11:53:59 -0400

    Create AssetManifest.js

---
+++ src/game/systems/AssetManifest.js
@@ -0,0 +1,68 @@
+export const IMAGE_ASSETS = [
+  // BACKGROUNDS
+  ["bg_cutscene_default", "/assets/backgrounds/bg_cutscene_default.png"],
+
+  ["bg_depth_1", "/assets/backgrounds/bg_depth_1.png"],
+  ["bg_depth_2", "/assets/backgrounds/bg_depth_2.png"],
+  ["bg_depth_3", "/assets/backgrounds/bg_depth_3.png"],
+  ["bg_depth_4", "/assets/backgrounds/bg_depth_4.png"],
+  ["bg_depth_5", "/assets/backgrounds/bg_depth_5.png"],
+
+  ["bg_hallway_1", "/assets/backgrounds/bg_hallway_1.png"],
+  ["bg_hallway_2", "/assets/backgrounds/bg_hallway_2.png"],
+  ["bg_hallway_3", "/assets/backgrounds/bg_hallway_3.png"],
+  ["bg_hallway_4", "/assets/backgrounds/bg_hallway_4.png"],
+  ["bg_hallway_5", "/assets/backgrounds/bg_hallway_5.png"],
+
+  ["bg_boss_door_level1", "/assets/backgrounds/bg_boss_door_level1.png"],
+  ["bg_boss_door_level2", "/assets/backgrounds/bg_boss_door_level2.png"],
+  ["bg_boss_door_level3", "/assets/backgrounds/bg_boss_door_level3.png"],
+  ["bg_boss_door_level4", "/assets/backgrounds/bg_boss_door_level4.png"],
+  ["bg_boss_door_level5", "/assets/backgrounds/bg_boss_door_level5.png"],
+
+  // EFFECTS
+  ["fx_arc_purple", "/assets/effects/fx_arc_purple.png"],
+  ["fx_burst_purple", "/assets/effects/fx_burst_purple.png"],
+  ["fx_flash_white", "/assets/effects/fx_flash_white.png"],
+  ["fx_ring_purple", "/assets/effects/fx_ring_purple.png"],
+  ["fx_slash_purple", "/assets/effects/fx_slash_purple.png"],
+  ["fx_trail_purple", "/assets/effects/fx_trail_purple.png"],
+  ["fx_wave_dual", "/assets/effects/fx_wave_dual.png"],
+
+  // ICONS
+  ["icon_buff_block", "/assets/icons/icon_buff_block.png"],
+  ["icon_buff_crit", "/assets/icons/icon_buff_crit.png"],
+  ["icon_buff_damage", "/assets/icons/icon_buff_damage.png"],
+  ["icon_buff_double_strike", "/assets/icons/icon_buff_double_strike.png"],
+  ["icon_buff_hp", "/assets/icons/icon_buff_hp.png"],
+  ["icon_buff_speed", "/assets/icons/icon_buff_speed.png"],
+
+  ["icon_corrupt_crit", "/assets/icons/icon_corrupt_crit.png"],
+  ["icon_corrupt_power", "/assets/icons/icon_corrupt_power.png"],
+  ["icon_corrupt_speed", "/assets/icons/icon_corrupt_speed.png"],
+
+  ["icon_reward_crit", "/assets/icons/icon_reward_crit.png"],
+  ["icon_reward_damage", "/assets/icons/icon_reward_damage.png"],
+  ["icon_reward_heal", "/assets/icons/icon_reward_heal.png"],
+  ["icon_reward_shield", "/assets/icons/icon_reward_shield.png"],
+
+  ["icon_room_corrupt", "/assets/icons/icon_room_corrupt.png"],
+  ["icon_room_enemy", "/assets/icons/icon_room_enemy.png"],
+  ["icon_room_safe", "/assets/icons/icon_room_safe.png"],
+  ["icon_room_scan", "/assets/icons/icon_room_scan.png"],
+  ["icon_room_trap", "/assets/icons/icon_room_trap.png"],
+  ["icon_room_treasure", "/assets/icons/icon_room_treasure.png"],
+];
+
+export const JSON_ASSETS = [
+  ["buffs", "/data/buffs.json"],
+  ["classes", "/data/classes.json"],
+  ["dialogue", "/data/dialogue.json"],
+  ["enemies", "/data/enemies.json"],
+  ["hallways", "/data/hallways.json"],
+  ["levels", "/data/levels.json"],
+  ["room_rewards", "/data/room_rewards.json"],
+  ["specials", "/data/specials.json"],
+  ["ui", "/data/ui.json"],
+  ["weapons", "/data/weapons.json"],
+];
